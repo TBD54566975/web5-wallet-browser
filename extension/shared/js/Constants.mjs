96b54e1ee94e14738c6e7dedd2126ae709ae6b07
Devin Rousso <drousso@tbd.email>
2023-01-09 17:28:42 -0700

    add a tab to the options page for debugging
    currently it allows for raw storage modification to make it easier to recover from accidental deletions, getting into a known good state for testing, etc.

--- extension/shared/js/Constants.mjs
+++ extension/shared/js/Constants.mjs
@@ -3,4 +3,6 @@ export const ActionCreateProfile = "create-profile";
 export const ActionDeletePermission = "delete-permission";
 export const ActionDeleteProfile = "delete-profile";
 
+export const DebugModifyStorage = "debug-modify-storage";
+
 export const PopupDWNRequestAccess = "DWNRequestAccess";
